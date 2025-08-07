b9fdf674706bba11f7f9e3e0c91b93b17b01a8d3
Seth Battis <sbattis@groton.org>
2025-08-07 13:15:14 -0400

    fix: handle nested directories in template

--- src/Scripts.php
+++ src/Scripts.php
@@ -20,32 +20,38 @@ class Scripts
         return self::$filesystem;
     }
 
+    /**
+     * @return string[]|false
+     */
+    private static function scandir(string $filePath): mixed
+    {
+        $files = scandir($filePath);
+        if ($files) {
+            return array_diff($files, ['.', '..']);
+        }
+        return $files;
+    }
+
     public static function installGAEFiles(Event $event): void
     {
         $projectPath = getcwd();
         $templatePath = Path::join(__DIR__, '/../template');
         $io = $event->getIO();
         $io->write('Checking Google App Engine configuration...');
-        foreach (scandir($templatePath) as $fileName) {
-            switch ($fileName) {
-                case '.':
-                case '..':
-                    break;
-                default:
-                    $src = Path::join($templatePath, $fileName);
-                    $dest = Path::join($projectPath, $fileName);
-                    if (!self::fileDataEqual($src, $dest)) {
-                        if (self::fs()->exists($dest)) {
-                            $backup = self::backupFile($dest);
-                            $io->write("Backed up $dest to $backup");
-                        }
-                        if (is_dir($src)) {
-                            self::fs()->mirror($src, $dest);
-                        } else {
-                            self::fs()->copy($src, $dest);
-                        }
-                        $io->write("Installed $dest");
-                    }
+        foreach (self::scandir($templatePath) as $fileName) {
+            $src = Path::join($templatePath, $fileName);
+            $dest = Path::join($projectPath, $fileName);
+            if (!self::fileDataEqual($src, $dest)) {
+                if (self::fs()->exists($dest)) {
+                    $backup = self::backupFile($dest);
+                    $io->write("Backed up $dest to $backup");
+                }
+                if (is_dir($src)) {
+                    self::fs()->mirror($src, $dest);
+                } else {
+                    self::fs()->copy($src, $dest);
+                }
+                $io->write("Installed $dest");
             }
         }
         $io->write("Google App Engine configuration matches groton-school/slim-gae-shim.");
@@ -54,29 +60,57 @@ class Scripts
     /**
      * @see https://stackoverflow.com/a/3060247
      */
-    private static function fileDataEqual($a, $b): bool
+    private static function fileDataEqual(string $a, string $b): bool
     {
-        // Check if filesize is different
-        if (filesize($a) !== filesize($b)) {
-            return false;
-        }
-
-        // Check if content is different
-        $ah = fopen($a, 'rb');
-        $bh = fopen($b, 'rb');
-
-        $result = true;
-        while (!feof($ah)) {
-            if (fread($ah, 8192) != fread($bh, 8192)) {
-                $result = false;
-                break;
+        if (is_dir($a)) {
+            if (is_dir($b)) {
+                $afiles = self::scandir($a);
+                $bfiles = self::scandir($b);
+                $result = true;
+                foreach ($afiles as $afile) {
+                    $i = array_search($afile, $bfiles);
+                    if ($i !== false) {
+                        $bfile = $bfiles[$i];
+                        $bfiles = array_splice($bfiles, $i, 1);
+                        if (!self::fileDataEqual(Path::join($a, $afile), Path::join($b, $bfile))) {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                if (count($bfiles) > 0) {
+                    return false;
+                }
+                return true;
+            } else {
+                return false;
             }
+        } elseif (is_dir($b)) {
+            return false;
+        } else {
+            // Check if filesize is different
+            if (filesize($a) !== filesize($b)) {
+                return false;
+            }
+
+            // Check if content is different
+            $ah = fopen($a, 'rb');
+            $bh = fopen($b, 'rb');
+
+            $result = true;
+            while (!feof($ah)) {
+                if (fread($ah, 8192) != fread($bh, 8192)) {
+                    $result = false;
+                    break;
+                }
+            }
+
+            fclose($ah);
+            fclose($bh);
+
+            return $result;
         }
-
-        fclose($ah);
-        fclose($bh);
-
-        return $result;
     }
 
 
