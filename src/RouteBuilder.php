da40d0c5e4cf38a5070f9c8bf8e446c83b396a60
Seth Battis <sbattis@groton.org>
2025-09-18 21:07:20 -0400

    feat: inject inner middleware into RouteBuilder

--- src/RouteBuilder.php
+++ src/RouteBuilder.php
@@ -6,17 +6,23 @@ namespace GrotonSchool\Slim\GAE;
 
 use GrotonSchool\Slim\GAE\Actions\EmptyAction;
 use GrotonSchool\Slim\Norms\RouteBuilderInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface;
 use Slim\Interfaces\RouteGroupInterface;
 
 class RouteBuilder implements RouteBuilderInterface
 {
-    public function define(App $app): RouteGroupInterface
+    public function define(App $app, ?MiddlewareInterface ...$innerMiddleware): RouteGroupInterface
     {
         // return an empty string on GAE start/stop requests
-        return $app->group('/_ah', function (RouteCollectorProxyInterface $ah) {
+        $group = $app->group('/_ah', function (RouteCollectorProxyInterface $ah) {
             $ah->get('/{action:.*}', EmptyAction::class);
         });
+
+        foreach ($innerMiddleware as $middleware) {
+            $group = $group->add($middleware);
+        }
+        return $group;
     }
 }
